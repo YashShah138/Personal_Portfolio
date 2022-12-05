f69fded1f73ba65bb7a68d0bded2aad0cca91a49
YashShah138 <89223735+YashShah138@users.noreply.github.com>
2022-12-05 15:30:36 -0800

    Calculator stuff

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calculator/CalculatorApiController.java
@@ -0,0 +1,25 @@
+package com.nighthawk.spring_portfolio.mvc.calculator;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/calculator")
+public class CalculatorApiController {
+
+    @GetMapping("/{expression}")
+    public ResponseEntity<String> getResult(@PathVariable String expression) {
+
+        // Returns jsonified result of expression with tokens and everything
+        Calculator a = new Calculator(expression);
+        String result = a.jsonify();
+        if (result != null) {
+            return new ResponseEntity<String>(result, HttpStatus.OK);
+        }
+
+        // Bad ID
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);       
+    }
+
+}
